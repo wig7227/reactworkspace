aa01c55b9c3e23422eae9ccf51f2c5d6d480fb7d
wig7227 <wig7227@naver.com>
2025-04-04 17:54:33 +0900

    0404

---
+++ 01.my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 주석
+  /*
+    여러줄 주석
+  */
+  return (  //주석
+    //주석
+    <>  {/*  주석  :  여러줄 일때는 반드시 최상위 태그가 있어야 한다 */}
+      <h1>React Test Page</h1>
+      <p>오늘부터 리액트 시작</p>
+      <h3>종료시간이 다가옵니다</h3>
+    
+      {/*  주석  */}
+    </>
+  );
+}
+
+export default App;
